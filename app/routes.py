130139eb398623cbecebf7233a1400d9d7523ec1
Zachary Kline <zkline888@gmail.com>
2019-10-07 13:55:28 -0400

    Committing the finished 2nd part of the flask tutorial. Added some fun messages too. Exciting

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,27 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Zachary'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Everything you know is a lie!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Eat the rich!'
+        },
+        {
+            'author': {'username': 'Johnny Rico'},
+            'body': 'The only good bug, is a dead bug!'
+        },
+        {
+            'author': {'username': 'Your Mom'},
+            'body': 'I love you very much, call me more.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
